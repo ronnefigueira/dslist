e17a4df2bdaa58eed2120b032781200466fbe5ed
Ronne Figueira <ronnefigueira777@gmail.com>
2025-03-11 00:58:45 -0300

    Game seed

--- src/main/java/com/gamelistproject/dslist/entities/Game.java
+++ src/main/java/com/gamelistproject/dslist/entities/Game.java
@@ -14,21 +14,27 @@ public class Game {
     private String title;
     private Integer gameYear;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String title, Integer gameYear, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer gameYear, String genre, Double score, String platforms, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.gameYear = gameYear;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -66,12 +72,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platform) {
+        this.platforms = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
